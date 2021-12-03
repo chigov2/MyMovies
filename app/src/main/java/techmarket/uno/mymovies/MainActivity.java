7391665ab87dd056e3b5894b059a0aad1312c79f
chigov2 <chigov@gmail.com>
2021-12-03 12:52:07 +0200

    23.49

--- app/src/main/java/techmarket/uno/mymovies/MainActivity.java
+++ app/src/main/java/techmarket/uno/mymovies/MainActivity.java
@@ -3,6 +3,12 @@ package techmarket.uno.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import techmarket.uno.mymovies.utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //String url = NetworkUtils.buildURL(NetworkUtils.POPULARITY,1).toString();
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.TOP_RATED,3);
+        if (jsonObject == null){
+            Toast.makeText(this, "null", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
+        }
+        Log.i("test",jsonObject.toString());
     }
 }
--- app/src/main/java/techmarket/uno/mymovies/utils/NetworkUtils.java
+++ app/src/main/java/techmarket/uno/mymovies/utils/NetworkUtils.java
@@ -1,9 +1,19 @@
 package techmarket.uno.mymovies.utils;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class NetworkUtils {
 
@@ -26,21 +36,21 @@ public class NetworkUtils {
     public static final int TOP_RATED = 1;
 
     //метод, который будет формировать запрос - возвращать будет URL
-    public static URL buildURL(int sortBy, int page){
+    private static URL buildURL(int sortBy, int page) {
         //получили https://api.themoviedb.org/3/discover/movie в виде адреса и
         // можем прикреплять к нему параметры
         URL result = null;
         String methodOfSort;
-        if (sortBy == POPULARITY){
+        if (sortBy == POPULARITY) {
             methodOfSort = SORT_BY_POPULARITY;
-        }else {
+        } else {
             methodOfSort = SORT_BY_TOP_RATED;
         }
         Uri uri = Uri.parse(BASE_URL).buildUpon()
-                .appendQueryParameter(PARAMS_API_KEY,API_KEY)
-                .appendQueryParameter(PARAMS_LANGUAGE,LANGUAGE_VALUE)
-                .appendQueryParameter(PARAMS_SORT_BY,methodOfSort)
-                .appendQueryParameter(PARAMS_PAGE,Integer.toString(page))
+                .appendQueryParameter(PARAMS_API_KEY, API_KEY)
+                .appendQueryParameter(PARAMS_LANGUAGE, LANGUAGE_VALUE)
+                .appendQueryParameter(PARAMS_SORT_BY, methodOfSort)
+                .appendQueryParameter(PARAMS_PAGE, Integer.toString(page))
                 .build();
         //вернуть URL
         try {
@@ -49,8 +59,62 @@ public class NetworkUtils {
             e.printStackTrace();
         }
         return result;
-
     }
 
+    //метод, который будет получать из JSON сети
+    public static JSONObject getJSONFromNetwork(int sortBy, int page){
+        JSONObject result = null;
+        URL url = buildURL(sortBy,page);
+        try {
+            result = new JSONLoadTask().execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
+    //метод - загружает данные из интернета
+    private static class JSONLoadTask extends AsyncTask<URL, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(URL... urls) {
+            //переменная, которую будем возвращать
+            JSONObject result = null;
+            //проверка
+            if (urls == null || urls.length == 0){
+                return null;
+            }
+            //соединение
+            HttpURLConnection connection = null;
+            //открываем соединение
+            try {
+                connection = (HttpURLConnection) urls[0].openConnection();
+                //соединение открыто - создаем поток ввода
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                //чтобы читать сразу строками
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                StringBuilder builder = new StringBuilder();
+                //читаем данные
+                String line = reader.readLine();
+                while (line != null){
+                    builder.append(line);
+                    line = reader.readLine();
+                }
+                result = new JSONObject(builder.toString());
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null){
+                connection.disconnect();
+                }
+            }
+
+            return result;
+        }
+    }
 }
+
